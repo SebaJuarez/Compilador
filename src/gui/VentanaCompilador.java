002d54f1c8aadcee833d37dddba2cc57b0707672
Lucas <luquirueda01@gmail.com>
2026-05-19 16:19:31 -0300

    fix

--- src/gui/VentanaCompilador.java
+++ src/gui/VentanaCompilador.java
@@ -67,7 +67,7 @@ public class VentanaCompilador extends JFrame {
     }
 
     // ── Main ───────────────────────────────────────────────────────────────────
-    static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {
